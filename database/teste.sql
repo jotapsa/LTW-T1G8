3fa5ffe3cb7151e4a1901062aec13c20b8d929a9
João Sá <up201506252@fe.up.pt>
2017-12-14 00:13:32 +0000

    Trigger, if an unchecked item is deleted and the rest are checked.

--- database/teste.sql
+++ database/teste.sql
@@ -1,16 +1,16 @@
---Trigger to check List if all items are checked
-CREATE TRIGGER IF NOT EXISTS LIST_UNCHECKED
-AFTER UPDATE ON Item
+--Trigger to check List if one unchecked item is deleted and the rest are checked
+CREATE TRIGGER IF NOT EXISTS ITEM_DELETED
+AFTER DELETE ON Item
 FOR EACH ROW --Already default
-WHEN ((new.checked = 0 AND old.checked = 1) AND
-(SELECT count(Item.idItem) FROM List
-  INNER JOIN Item
-  ON ((new.idList = List.idList) AND (Item.idList = List.idList) AND (Item.checked = 1))) <
+WHEN ((old.checked = 0) AND
   (SELECT count(Item.idItem) FROM List
   INNER JOIN Item
-  ON ((new.idList = List.idList) AND (Item.idList = List.idList))))
+  ON ((old.idList = List.idList) AND (Item.idList = List.idList) AND (Item.checked = 1))) =
+  (SELECT count(Item.idItem) FROM List
+  INNER JOIN Item
+  ON ((old.idList = List.idList) AND (Item.idList = List.idList))))
 BEGIN
   UPDATE List
-  SET checked = 0
-  WHERE (idList = new.idList);
+  SET checked = 1
+  WHERE (idList = old.idList);
 END;
